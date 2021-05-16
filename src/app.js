fb6cda9ab27b8a4cff46d5cb217c1018135c0c80
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-05-16 00:25:51 -0300

    refactor: update app

--- src/app.js
+++ src/app.js
@@ -12,4 +12,20 @@ app.get('/v1/cep/:term', async (req, res) => {
   res.json(data);
 });
 
+app.get('/', (req, res) => {
+  const wellcome = `
+  <h1>Acessando o webservice de CEP dos correios</h1>
+
+  <p>Para acessar o webservice, um CEP no formato de {8} digitos ou um endereco deve ser fornecido, por exemplo: "01001000" ou "0100"</p>
+
+  <p>Exemplo de pesquisa por CEP: <a href="http://localhost:3000/v1/cep/68900075" target="__blank">http://localhost:3000/v1/cep/68900075</a></p>
+
+  <p>Exemplo de pesquisa por Endereco: <a href="http://localhost:3000/v1/cep/1852" target="__blank">http://localhost:3000/v1/cep/1852</a></p>
+
+  <p>Caso tenha duvidas utilize a plataforma dos correios <a href="https://buscacepinter.correios.com.br/app/endereco/index.php?t" target="__blank">https://buscacepinter.correios.com.br/app/endereco/index.php?t</a> para facilitar o entendimento.</p>
+  `;
+
+  res.end(wellcome)
+})
+
 module.exports = app;
