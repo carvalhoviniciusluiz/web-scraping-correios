0ab9c1767efa3d0642f133c40064bac2c0252922
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-05-16 15:36:46 -0300

    refector: add root api, add html engine and add public folder

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const app = require('./src/app')
+const express = require('express');
+const cons = require('consolidate');
 
+const app = require('./src/app');
 const port = 3000
 
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('page');
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
