7f4ab786cec43902b8504e83b74cf329fd2fd561
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-05-17 12:46:04 -0300

    refactor: add new api

--- src/app.js
+++ src/app.js
@@ -14,4 +14,18 @@ app.get('/v1/cep/:value', async (req, res) => {
   res.json(data);
 });
 
+app.get('/v2/cep/:value', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+
+  const { value } = req.params;
+
+  try {
+    const data = await Correios.fetchBySoapService(value);
+    res.json(data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
 module.exports = app;
